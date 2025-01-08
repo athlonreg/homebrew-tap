fea173d4120871ea7da9ce5dd8e193ecb4090c20
athlonreg <15563836030@163.com>
2025-01-08 17:54:48 +0800

    fix image-sync

--- image-sync.rb
+++ image-sync.rb
@@ -3,7 +3,7 @@ class ImageSync < Formula
   homepage ""
   url "https://raw.githubusercontent.com/athlonreg/homebrew-tap/master/src/image-sync.tar.xz"
   version "1.1"
-  sha256 "d195f529ff2c22be694a310fa8a103701aa4cdc8fc6bfe00c0f387d52df8c8d6"
+  sha256 "79626908da129b5443565f5767be09c2baf316de44b2ce6e2e501c0c6075cb6a"
 
   def install
     bin.install "image-sync"
