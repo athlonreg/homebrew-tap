cc03136df0a51d73e2929f26e6bf2bd19141c531
athlonreg <15563836030@163.com>
2025-01-08 17:50:47 +0800

    fix image-sync

--- image-sync.rb
+++ image-sync.rb
@@ -2,7 +2,7 @@ class ImageSync < Formula
   desc "Command line tool to sync image from one container registry to another use skopeo"
   homepage ""
   url "https://raw.githubusercontent.com/athlonreg/homebrew-tap/master/src/image-sync.tar.xz"
-  version "1.0"
+  version "1.1"
   sha256 "d195f529ff2c22be694a310fa8a103701aa4cdc8fc6bfe00c0f387d52df8c8d6"
 
   def install
